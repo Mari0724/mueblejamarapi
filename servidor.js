47abfe9c560eb48783da943e7ec301ce74da3e99
Mari0724 <ximenadelgadom07@gmail.com>
2025-04-22 11:50:05 -0500

    continuacion 22/04/2025

--- servidor.js
+++ servidor.js
@@ -1,16 +1,30 @@
 const express = require('express');
 const cors = require('cors');
 const rutacliente = require('./vista/ClienteRutas');
-// const path = require('path');
+const path = require('path');
 const app = express();
 
 
-//Middleware
+
+app.use(cors({
+    origin: '*', // Cambiar ['http://tu.com', 'http://yo.com'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Métodos permitidos
+    allowedHeaders: ['Content-Type', 'Authorization'], // Encabezados permitidos
+    credentials: true // Habilita el envío de credenciales si es necesario
+  }));
+
+
+
+// Middleware para parseo de solicitudes
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/images', express.static(path.join(__dirname, 'images')));
-app.use(bodyParser.json()); //jnson a html
-app.use(cors());
-app.use('/', indexDia);
+
+
+// Rutas 
+app.use('/', rutacliente);
+
+
 
 //crear el servidor
 const PORT = 3030
